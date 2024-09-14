80eb2db8d0814b9afc179fb80298a86eb319a2be
Nick_ituk <nick_ituk@yandex.ru>
2024-09-14 09:38:41 +0300

    added tqdm progress bar to jackknife method

--- core/utils.py
+++ core/utils.py
@@ -12,7 +12,7 @@ def jackknife(samples: np.ndarray):
     """Return mean and estimated lower error bound."""
     means = []
 
-    for i in range(samples.shape[0]):
+    for i in tqdm(range(samples.shape[0])):
         means.append(np.delete(samples, i, axis=0).mean(axis=0))
 
     means = np.asarray(means)
